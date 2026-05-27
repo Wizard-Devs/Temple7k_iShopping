304347d6169d0828cf6ae59311765010f85b3924
rovierrr <brunomoura.bm2007@gmail.com>
2026-05-27 10:38:32 +0100

    Login Form

--- Temple7k_iShopping/Form1.Designer.cs
+++ Temple7k_iShopping/Form1.Designer.cs
@@ -30,12 +30,16 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.textBox1Username = new System.Windows.Forms.TextBox();
+            this.textBox2Password = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(42, 39);
+            this.label1.Location = new System.Drawing.Point(11, 15);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(58, 13);
             this.label1.TabIndex = 0;
@@ -44,17 +48,55 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(42, 97);
+            this.label2.Location = new System.Drawing.Point(11, 73);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(56, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.label2.Text = "Password:";
+            // 
+            // textBox1Username
+            // 
+            this.textBox1Username.Location = new System.Drawing.Point(75, 12);
+            this.textBox1Username.Name = "textBox1Username";
+            this.textBox1Username.Size = new System.Drawing.Size(175, 20);
+            this.textBox1Username.TabIndex = 2;
+            // 
+            // textBox2Password
+            // 
+            this.textBox2Password.Location = new System.Drawing.Point(73, 66);
+            this.textBox2Password.Name = "textBox2Password";
+            this.textBox2Password.Size = new System.Drawing.Size(175, 20);
+            this.textBox2Password.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(73, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Entrar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(175, 104);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Cancelar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(348, 152);
+            this.ClientSize = new System.Drawing.Size(278, 152);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2Password);
+            this.Controls.Add(this.textBox1Username);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -68,6 +110,10 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1Username;
+        private System.Windows.Forms.TextBox textBox2Password;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
